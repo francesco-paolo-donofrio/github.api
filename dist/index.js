7b957c9fa936828921a3e9b765c11c9c433450ba
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-15 18:30:51 +0200

    Setting up the Developing Environment

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 0;
+if (age < 50) {
+    age = 18;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 0;
+
+if (age < 50) {
+    age = 18;
+}
+console.log(age);
